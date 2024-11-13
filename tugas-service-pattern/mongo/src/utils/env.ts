b255e9e0b5dfed5c0de4b3d3969077f775bc1d14
Alzi <aljisaptiadi@gmail.com>
2024-11-13 22:30:23 +0700

    tugas-service-pattern

---
+++ tugas-service-pattern/mongo/src/utils/env.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_SECRET: string =
+  process.env.CLOUDINARY_API_SECRET || "";
+export const CLOUDINARY_CLOUD_NAME: string =
+  process.env.CLOUDINARY_CLOUD_NAME || "";
+export const DATABASE_URL: string = process.env.DATABASE_URL || "mongodb+srv://aljisaptiadi:vmZwSrqW4tlG4lq1@cluster0.fkezp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
